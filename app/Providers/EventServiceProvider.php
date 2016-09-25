03021c3145def8264e55a99e6c249e7420374482
DylanH <dharrington2012@yahoo.com>
2016-09-25 17:30:56 -0400

    sdasd

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -27,10 +27,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot(DispatcherContract $events) {
         
     }
-    public function boot()
-    {
+
         parent::boot($events);
-        parent::boot();
         //
-    }
 }
